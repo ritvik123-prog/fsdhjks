d7d78cdd5cfe90e4d7772586e5363027fff40f44
ritvik123-prog <77570013+ritvik123-prog@users.noreply.github.com>
2021-08-16 19:01:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var hypnoticBall, database;
+var position;
+var gameState=0
+var playerCount=0
+var form;
+var game, player;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game=new Game()
+  game.getState()
+  game.start()
+
+}
+
+function draw(){
+  background("white");
+  
+   
+    drawSprites();
+  
+}
+
+
